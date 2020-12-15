4761f3c6c489afbfaf6acc1ed83b831d0895acf8
ceramicsamurai <76029969+ceramicsamurai@users.noreply.github.com>
2020-12-15 13:48:45 +0400

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Common;
+using System.Data.SQLite;
+using System.IO;
+
+
+namespace WindowsFormsApp2
+{
+       public partial class Form1 : Form
+    {
+        private String dbFileName;
+        private SQLiteConnection m_dbConn;
+        private SQLiteCommand m_sqlCmd;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            m_dbConn = new SQLiteConnection();
+            m_sqlCmd = new SQLiteCommand();
+
+            dbFileName = "database";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(dbFileName))
+                SQLiteConnection.CreateFile(dbFileName);
+
+            try
+            {
+                m_dbConn = new SQLiteConnection("Data Source=" + dbFileName + ";Version=3;");
+                m_dbConn.Open();
+                m_sqlCmd.Connection = m_dbConn;
+
+                m_sqlCmd.CommandText = "CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY AUTOINCREMENT, ФИО TEXT, Группа TEXT, Балл TEXT)";
+                m_sqlCmd.ExecuteNonQuery();
+
+            }
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!File.Exists(dbFileName))
+                MessageBox.Show("Нажмите кнопку Создать");
+
+            try
+            {
+                m_dbConn = new SQLiteConnection("Data Source=" + dbFileName + ";Version=3;");
+                m_dbConn.Open();
+                m_sqlCmd.Connection = m_dbConn;    
+            }
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DataTable dTable = new DataTable();
+            String sqlQuery;
+
+            if (m_dbConn.State != ConnectionState.Open)
+            {
+                MessageBox.Show("Соединитесь с базой");
+                return;
+            }
+
+            try
+            {
+                sqlQuery = "SELECT * FROM Catalog";
+                SQLiteDataAdapter adapter = new SQLiteDataAdapter(sqlQuery, m_dbConn);
+                adapter.Fill(dTable);
+
+                if (dTable.Rows.Count > 0)
+                {
+                    dataGridView1.Rows.Clear();
+
+                    for (int i = 0; i < dTable.Rows.Count; i++)
+                        dataGridView1.Rows.Add(dTable.Rows[i].ItemArray);
+                }
+                else
+                    MessageBox.Show("База пуста");
+            }
+            catch (SQLiteException ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (m_dbConn.State != ConnectionState.Open)
+            {
+                MessageBox.Show("Соединитесь с БД");
+                return;
+            }
+
+            Form2 addData = new Form2();
+            if (addData.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    m_sqlCmd.CommandText = "INSERT INTO students ('ФИО', 'Группа', 'Балл') values ('" +addData.ФИО + "' , '" + addData.Группа + "', '"+addData.Балл+"')";
+
+                    m_sqlCmd.ExecuteNonQuery();
+                }
+                catch (SQLiteException ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+    }
+ 
+}
---
+++ Form2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form2 : Form
+    {
+        public String ФИО { get; set; }
+        public String Группа { get; set; }
+        public String Балл { get; set; }
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ФИО = textBox1.Text;
+            Группа = textBox2.Text;
+            Балл = textBox3.Text;
+
+            DialogResult = DialogResult.OK;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            ФИО = "ФИО";
+            Группа = "Группа";
+            Балл = "Средний балл";
+
+            textBox1.Text = ФИО;
+            textBox2.Text = Группа;
+            textBox3.Text = Балл;      
+        }
+    }
+}
